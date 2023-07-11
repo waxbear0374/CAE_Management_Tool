f235e4fdf04317c46a4258c334da3a19373482a6
Heesub <waxbear@gmail.com>
2023-07-12 00:44:31 +0900

    Add Project Frame - Project Name

--- main.py
+++ main.py
@@ -5,8 +5,40 @@ from tkinter import ttk
 
 #from project_add import AddProject
 def OnAddProjectLayout():
+    win_add_project = tk.Tk()
+    win_add_project.geometry("500x400")
+    win_add_project.title("Add Project")
+    
+    #-- Style 지정
+    style = ttk.Style(win_add_project)
+    win_add_project.tk.call("source", "forest-light.tcl")
+    win_add_project.tk.call("source", "forest-dark.tcl")
+    style.theme_use("forest-dark")
+
+    #-- Main Frame
+    label_frame_add_project = ttk.Labelframe(win_add_project, text="Add Project")
+    label_frame_add_project.pack(side="top", fill="both", expand=True)
+
+    #-------------------------------------
+    #-- Project Name
+    #-------------------------------------
+    #-- Frame
+    frame_project_name = ttk.Frame(label_frame_add_project)
+    frame_project_name.pack(side="top", fill="both")
+
+    #-- Label
+    label_project_name = ttk.Label(frame_project_name, text="Project Name :", style="BW.TLabel")
+    label_project_name.pack(side="left", padx=2)
+
+    #-- Entry
+    entry_prject_name = ttk.Entry(frame_project_name)
+    entry_prject_name.pack(side="left")
+
     print("ADD")
-    #messagebox.showinfo("Button Clicked", "Somebody clicked!") 
+    #messagebox.showinfo("Button Clicked", "Somebody clicked!")
+
+    win_add_project.mainloop()
+
 
 #-- Main Setting
 root = tk.Tk()
